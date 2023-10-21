e9e746d129328c419a36f12735dbf4092652310d
SerizawaBeta-MD <131010614+SerizawaBeta-MD@users.noreply.github.com>
2023-10-21 06:37:13 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 # Código desarrollado por @gata_dios 
   
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="GataBot-MD"  
+BOT_DIR="PruebaBot-MD"  
 BOT_REPO="https://github.com/SerizawaBeta-MD/$BOT_DIR"
 DB_FILE="database.json"
  
